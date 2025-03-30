bf1c0b83f8dd4d8c29bad3254846cf9a7a47977c
tomoyuki65 <tomoyuki65@users.noreply.github.com>
2025-03-31 00:57:31 +0900

    add custom validator

--- src/util/validator/validator.go
+++ src/util/validator/validator.go
@@ -1,6 +1,9 @@
 package validator
 
 import (
+	"strconv"
+	"fmt"
+
 	"github.com/go-playground/validator/v10"
 )
 
@@ -8,13 +11,45 @@ type customValidator struct {
 	validator *validator.Validate
 }
 
+// カラムのバイト数チェック
+func validateColumnByteSize(fl validator.FieldLevel) bool {
+	value := fl.Field().String()
+	param := fl.Param()
+
+	checkByte, err := strconv.Atoi(param)
+	if err != nil {
+		return false
+	}
+
+	return len(value) <= checkByte
+}
+
 func (cv *customValidator) Validate(i interface{}) error {
-	// 必要であれば、ここでカスタムバリデーションルールを追加
-	return cv.validator.Struct(i)
+	err := cv.validator.Struct(i)
+	if err != nil {
+		// カスタムエラーメッセージの設定
+		var errMsg string
+		for _, err := range err.(validator.ValidationErrors) {
+			switch err.ActualTag() {
+			case "byte-size":
+				errMsg += fmt.Sprintf("%sは%sバイト以下で入力して下さい。", err.Field(), err.Param())
+			default:
+				errMsg += err.Error()
+			}
+		}
+
+		return fmt.Errorf(errMsg)
+	}
+
+	return nil
 }
 
 func NewCustomValidator() *customValidator {
+	v := validator.New()
+	// カスタムバリデーション設定
+	v.RegisterValidation("byte-size", validateColumnByteSize)
+
 	return &customValidator{
-		validator: validator.New(),
+		validator: v,
 	}
 }
